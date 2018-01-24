7675946be8d64f16f143696a82f0c56f3544fdf1
goncharovsu <goncharovsu@yandex.ru>
2018-01-25 01:08:53 +0300

    Создал проект и разместил код сайта для обучения

---
+++ lesson4/config/config.php
@@ -0,0 +1,14 @@
+
+<?php 
+define('IMG_DIR',"../htdocs/img/");    // Путь к картинкам
+define('IMG_TYPES',["jpg", "png", "gif"]);   // Разрешеные типы изображений
+define('IMG_TABLE','galery');  // Название таблицы в базе данных
+define('DB_HOST','localhost'); // Адрес подключения к базе
+define('DB_NAME','geekshop_2017'); // Название базы данных
+define('DB_USER','php_user_GoncharovSU'); // Пользователь базы данных
+define('DB_PASS','pass');  // Пароль к базе данных
+define('DB_INIT',FALSE);  // Флаг пересоздания базы
+// hjhjh
+?>
+
+
